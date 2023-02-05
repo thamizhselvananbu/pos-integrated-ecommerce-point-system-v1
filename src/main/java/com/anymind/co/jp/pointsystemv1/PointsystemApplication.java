1352b311e00a8113baea8bea9b05d250dd1fa700
thamizhselvananbu <anbuthamizhselvan@gmail.com>
2023-02-06 06:16:11 +0900

    Initial Commit for Point system API

---
+++ src/main/java/com/anymind/co/jp/pointsystemv1/PointsystemApplication.java
@@ -0,0 +1,13 @@
+package com.anymind.co.jp.pointsystemv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PointsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anymind/co/jp/pointsystemv1/PointsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anymind.co.jp.pointsystemv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
